f163feb8d698b303b6800c3cbb10b246517fa854
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-08-20 12:59:52 -0300

    feat: Create JWT Authentication from BackEnd

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,9 +1,14 @@
+const jwt = require("jsonwebtoken");
+
 function doLogin(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
   if (email === "brunoemf@gmail.com" && password === "123456") {
-    res.sendStatus(200);
+    const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET, {
+      expiresIn: parseInt(process.env.JWT_EXPIRES),
+    });
+    res.json({ token });
   } else res.sendStatus(401);
 }
 
