3e43fdd5546b6ae176ab43c7ac4cb292b4ebc9d8
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-08-20 10:33:04 -0300

    chore: Setup Middlewares cors, helmet and express.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const cors = require('cors');
+const helmet = require('helmet');
 
 // Carregamento do Aplicativo
 const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(express.json());
 
 // Configurações do Backend ( feita a partir de middlewares )
 app.use("/", (req, res, next) => {
   res.send("hello wordl");
 });
+
+
 
 module.exports = app;
