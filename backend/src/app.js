b448225bd7dfadcf7242f7b54ebdebd833c00933
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-08-20 11:18:52 -0300

    feat: Login route

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,9 +12,19 @@ app.use(helmet());
 
 app.use(express.json());
 
-// Configurações do Backend ( feita a partir de middlewares )
-app.use("/", (req, res, next) => {
-  res.send("hello wordl");
+// **** Configurações do Backend ( feita a partir de middlewares ) ****
+
+app.post("/login", (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email === "brunoemf@gmail.com" && password === "123456") {
+    res.sendStatus(200);
+  } else res.sendStatus(401);
+});
+
+app.post("/logout", (req, res, next) => {
+  res.sendStatus(200);
 });
 
 // Middleware de erros
