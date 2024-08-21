2953c1a8ba163de394c0114bb5472761954f1995
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-08-20 22:30:34 -0300

    feat: Authentication Middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,9 @@ require("express-async-errors");
 const cors = require("cors");
 const helmet = require("helmet");
 
-const authController = require("./controllers/authController");
+const { doLogin, doLogout } = require("./controllers/authController");
+const { getSettings } = require("./controllers/settingsController");
+const authMiddleware = require("./middlewares/authMiddleware");
 
 // Carregamento do Aplicativo
 const app = express();
@@ -15,9 +17,11 @@ app.use(helmet());
 app.use(express.json());
 
 // **** Configurações do Backend ( feita a partir de middlewares ) ****
-app.post("/login", authController.doLogin);
+app.get("/settings", authMiddleware, getSettings);
 
-app.post("/logout", authController.doLogout);
+app.post("/login", doLogin);
+
+app.post("/logout", doLogout);
 
 // Middleware de erros
 app.use(require("./middlewares/errorMiddleware"));
