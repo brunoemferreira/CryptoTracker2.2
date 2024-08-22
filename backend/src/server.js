d143f0988de6cdf152fa4f0c823609fe14c6f6ee
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-08-22 07:24:03 -0300

    chore: Sequelize Initial Setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,8 @@
+// O app.js sobe o Express
 const app = require("./app");
+
+// O db.js sobe o sequelize
+const db = require("./db")
 
 app.listen(process.env.PORT, () => {
   console.log("🚀 App is running at "+ process.env.PORT);
