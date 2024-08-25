332a93d0432a31712f90cd2dc3e09b83565c0f0b
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-08-24 23:01:56 -0300

    feat: Created Menus and NavBar

---
+++ frontend/src/components/Menu/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {NavBar} from "./NavBar";
+import {SideBar} from "./SideBar";
+
+export default function Menu() {
+  return (
+    <>
+      <NavBar />
+      <SideBar />
+    </>
+  );
+}
