83b04080f1b8a055b9522cf46fea94d225cd7fbe
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-08-27 11:12:19 -0300

    feat: Finalized the front and back settings

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,20 +4,28 @@ const cors = require("cors");
 const helmet = require("helmet");
 
 const { doLogin, doLogout } = require("./controllers/authController");
-const { getSettings } = require("./controllers/settingsController");
+const {
+  getSettings,
+  updateSettings,
+} = require("./controllers/settingsController");
 const authMiddleware = require("./middlewares/authMiddleware");
+const morgan = require("morgan");
 
 // Carregamento do Aplicativo
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 app.use(helmet());
 
 app.use(express.json());
 
+app.use(morgan("dev"));
+
 // **** Configurações do Backend ( feita a partir de middlewares ) ****
 app.get("/settings", authMiddleware, getSettings);
+
+app.patch("/settings", authMiddleware, updateSettings);
 
 app.post("/login", doLogin);
 
